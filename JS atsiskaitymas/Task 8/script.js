84be646a37261f897f9ca6f2502a2de7396ccf73
Tudubum <gdeveikyte95@gmail.com>
2023-01-02 11:58:56 +0200

    8 done

--- JS atsiskaitymas/Task 8/script.js
+++ JS atsiskaitymas/Task 8/script.js
@@ -5,3 +5,25 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator{
+    sum(sk1,sk2){
+        return sk1 + sk2
+    }
+    subtraction(sk1,sk2){
+        return sk1 - sk2;
+    }
+    multiplication(sk1, sk2){
+        return sk1 * sk2;
+    }
+    division(sk1, sk2){
+        return sk1 / sk2;
+    }
+        
+}
+
+calc = new Calculator()
+console.log(calc.sum(4,4));
+console.log(calc.subtraction(6,6));
+console.log(calc.multiplication(6,6));
+console.log(calc.division(12,2));
