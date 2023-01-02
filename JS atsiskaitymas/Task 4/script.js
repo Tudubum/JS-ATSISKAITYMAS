92d0701dfb469267f3b9dab4703b41e7806acff9
Tudubum <gdeveikyte95@gmail.com>
2023-01-02 14:28:28 +0200

    4 done ir 10 papildyta

--- JS atsiskaitymas/Task 4/script.js
+++ JS atsiskaitymas/Task 4/script.js
@@ -9,3 +9,36 @@ Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
+function getData(){
+    fetch('cars.json')
+        .then(response => response.json()
+        .then(data => {
+            data.forEach(car => {
+                const brand = car.brand;
+                const models = car.models;
+
+                const card = document.createElement('div');
+                card.classList.add('card')
+
+                const brandElement = document.createElement('h2');
+                brandElement.textContent = brand;
+
+                const modelsElement = document.createElement('ol');
+                models.forEach(model => {
+                    const modelItem = document.createElement('li')
+                    modelItem.textContent = model;
+                    modelsElement.appendChild(modelItem);
+                });
+
+                card.appendChild(brandElement);
+
+                card.appendChild(modelsElement);
+          
+                document.body.appendChild(card);
+
+                output.appendChild(card);
+            });
+        }));
+}
+
+window.addEventListener('load', getData);
