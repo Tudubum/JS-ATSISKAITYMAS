e16f614ba7042db4193271065173df4646154fb7
Tudubum <gdeveikyte95@gmail.com>
2023-01-02 13:15:15 +0200

    9 done

--- JS atsiskaitymas/Task 9/script.js
+++ JS atsiskaitymas/Task 9/script.js
@@ -6,3 +6,22 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director, budget){
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpensive(budget){
+        return this.budget > 100000000;
+    }
+}
+
+let movieList = [
+    new Movie("Avatar", "James Cameron", 237000000),
+    new Movie("Titanikas", "James Cameron", 200000000),
+    new Movie("Matrica", "Lana Wachowski", 90000000)
+]
+
+console.log(movieList[2].wasExpensive());
